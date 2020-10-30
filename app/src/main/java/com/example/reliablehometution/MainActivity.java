d0b31cef764d2b6ab741b956e93642ce899faa64
balaram714 <nityanandasamanta96@gmail.com>
2020-10-30 22:25:39 +0530

    LoginPage added to the app

--- app/src/main/java/com/example/reliablehometution/MainActivity.java
+++ app/src/main/java/com/example/reliablehometution/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.reliablehometution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView teacherimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teacherimage = (ImageView) findViewById(R.id.teacherImage);
+        teacherimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginPageTeacher.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
