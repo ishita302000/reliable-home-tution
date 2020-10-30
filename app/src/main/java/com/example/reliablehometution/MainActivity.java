c2726b03930f607d28d2157ee1b35fe3a0847aca
balaram714 <nityanandasamanta96@gmail.com>
2020-10-30 22:53:15 +0530

    LoginPage for student added to the app

--- app/src/main/java/com/example/reliablehometution/MainActivity.java
+++ app/src/main/java/com/example/reliablehometution/MainActivity.java
@@ -9,18 +9,28 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView teacherimage;
+    private ImageView studentimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         teacherimage = (ImageView) findViewById(R.id.teacherImage);
+        studentimage = (ImageView) findViewById(R.id.childrenImage);
         teacherimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginPageTeacher.class);
-                startActivity(intent);
+                Intent intent_teacher = new Intent(MainActivity.this, LoginPageTeacher.class);
+                startActivity(intent_teacher);
 
+            }
+        });
+
+        studentimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_student = new Intent(MainActivity.this, LoginPageStudent.class);
+                startActivity(intent_student);
             }
         });
     }
