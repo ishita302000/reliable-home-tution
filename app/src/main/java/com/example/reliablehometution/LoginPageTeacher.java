b40464e2c6944629ff3bf86e2416a6b482d88d43
balaram714 <nityanandasamanta96@gmail.com>
2020-10-31 23:54:12 +0530

    added signup for teacher in the project

--- app/src/main/java/com/example/reliablehometution/LoginPageTeacher.java
+++ app/src/main/java/com/example/reliablehometution/LoginPageTeacher.java
@@ -2,13 +2,27 @@ package com.example.reliablehometution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPageTeacher extends AppCompatActivity {
 
+    private TextView signUp_LoginPageTeacher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page_teacher);
+
+        signUp_LoginPageTeacher = findViewById(R.id.slogan_name_teacher);
+
+        signUp_LoginPageTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginPageTeacher.this,SignUpTeacher.class));
+            }
+        });
     }
 }
