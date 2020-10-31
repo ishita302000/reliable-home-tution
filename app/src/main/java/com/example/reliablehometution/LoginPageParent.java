2204e3918b46c3c44cd3a8b3c21f2b84bea9f0fd
balaram714 <nityanandasamanta96@gmail.com>
2020-11-01 01:25:00 +0530

    added signup for parent in the project

--- app/src/main/java/com/example/reliablehometution/LoginPageParent.java
+++ app/src/main/java/com/example/reliablehometution/LoginPageParent.java
@@ -2,13 +2,27 @@ package com.example.reliablehometution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPageParent extends AppCompatActivity {
 
+    private TextView signUp_LoginPageParent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page_parent);
+
+        signUp_LoginPageParent = findViewById(R.id.slogan_name_parent);
+
+        signUp_LoginPageParent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginPageParent.this,SignUpParent.class));
+            }
+        });
+
     }
 }
