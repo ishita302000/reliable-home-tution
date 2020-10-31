1ee61df9d6a27f56b14f083e5dbb62a89b299d50
balaram714 <nityanandasamanta96@gmail.com>
2020-11-01 00:23:14 +0530

    added signup for student in the project

--- app/src/main/java/com/example/reliablehometution/LoginPageStudent.java
+++ app/src/main/java/com/example/reliablehometution/LoginPageStudent.java
@@ -2,13 +2,26 @@ package com.example.reliablehometution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPageStudent extends AppCompatActivity {
 
+    private TextView signUp_LoginPageStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page_student);
+
+        signUp_LoginPageStudent = findViewById(R.id.slogan_name_student);
+
+        signUp_LoginPageStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginPageStudent.this,SignUpStudent.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/reliablehometution/SignUpStudent.java
@@ -0,0 +1,14 @@
+package com.example.reliablehometution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_student);
+    }
+}
