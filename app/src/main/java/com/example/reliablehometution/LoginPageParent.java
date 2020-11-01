cfd31530b2cac8bc7b9b668d96a4d27344fe4458
balaram714 <nityanandasamanta96@gmail.com>
2020-11-01 11:36:30 +0530

    added signIn for parent phone in the project

--- app/src/main/java/com/example/reliablehometution/LoginPageParent.java
+++ app/src/main/java/com/example/reliablehometution/LoginPageParent.java
@@ -10,12 +10,14 @@ import android.widget.TextView;
 public class LoginPageParent extends AppCompatActivity {
 
     private TextView signUp_LoginPageParent;
+    private TextView login_with_phoneno_LoginPageParent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page_parent);
 
         signUp_LoginPageParent = findViewById(R.id.slogan_name_parent);
+        login_with_phoneno_LoginPageParent = findViewById(R.id.phnno_log_parent);
 
         signUp_LoginPageParent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +26,11 @@ public class LoginPageParent extends AppCompatActivity {
             }
         });
 
+        login_with_phoneno_LoginPageParent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginPageParent.this,LoginParentPhone.class));
+            }
+        });
     }
 }
