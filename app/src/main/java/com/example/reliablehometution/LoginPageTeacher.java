f0dbad1f485166ccb59b924f661f3d9a0491b808
balaram714 <nityanandasamanta96@gmail.com>
2020-11-01 11:05:24 +0530

    added signIn for teacher phone in the project

--- app/src/main/java/com/example/reliablehometution/LoginPageTeacher.java
+++ app/src/main/java/com/example/reliablehometution/LoginPageTeacher.java
@@ -11,12 +11,15 @@ import android.widget.TextView;
 public class LoginPageTeacher extends AppCompatActivity {
 
     private TextView signUp_LoginPageTeacher;
+    private TextView login_with_phoneno_LoginPageTeacher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page_teacher);
 
         signUp_LoginPageTeacher = findViewById(R.id.slogan_name_teacher);
+        login_with_phoneno_LoginPageTeacher = findViewById(R.id.phnno_log_teacher);
+
 
         signUp_LoginPageTeacher.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +27,13 @@ public class LoginPageTeacher extends AppCompatActivity {
                 startActivity( new Intent(LoginPageTeacher.this,SignUpTeacher.class));
             }
         });
+
+        login_with_phoneno_LoginPageTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginPageTeacher.this,LoginTeacherPhone.class));
+            }
+        });
     }
+
 }
