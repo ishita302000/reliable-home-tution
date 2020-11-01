ed8083fe07338bffb0e2cffa99657eded63f5452
balaram714 <nityanandasamanta96@gmail.com>
2020-11-01 11:22:46 +0530

    added signIn for student phone in the project

--- app/src/main/java/com/example/reliablehometution/LoginPageStudent.java
+++ app/src/main/java/com/example/reliablehometution/LoginPageStudent.java
@@ -10,17 +10,27 @@ import android.widget.TextView;
 public class LoginPageStudent extends AppCompatActivity {
 
     private TextView signUp_LoginPageStudent;
+    private TextView login_with_phoneno_LoginPageStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page_student);
 
         signUp_LoginPageStudent = findViewById(R.id.slogan_name_student);
+       login_with_phoneno_LoginPageStudent = findViewById(R.id.phnno_log_student);
+
 
         signUp_LoginPageStudent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity( new Intent(LoginPageStudent.this,SignUpStudent.class));
+            }
+        });
+
+        login_with_phoneno_LoginPageStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginPageStudent.this,LoginStudentPhone.class));
             }
         });
     }
